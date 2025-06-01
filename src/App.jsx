bb0f12d9430a2a1f5137d09240b0e04f167181ed
Pallob Barman <pallabbarman02@gmail.com>
2025-06-02 00:32:09 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Technologies from "./components/Technologies";
 const App = () => {
   return (
     <Router>
-  <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 h-[100dvh] relative">
+  <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 min-h-screen relative">
     
     <div className="fixed inset-0 -z-20 bg-neutral-950"></div>
     <div className="fixed inset-0 -z-30 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
